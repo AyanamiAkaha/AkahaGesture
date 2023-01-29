628e35a98590a08201d024c2ccd41985f2b39f93
Ayanami Akaha <ayanami@akaha.today>
2023-01-30 02:01:54 +0900

    Restore window position on startup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Akaha_Gesture {
     /// <summary>
@@ -19,11 +10,66 @@ namespace Akaha_Gesture {
     /// </summary>
 
     public partial class MainWindow : Window {
-        List<string> fileNames = new List<string>();
-
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            base.OnClosing(e);
+            var placement = this.GetPlacement();
+            byte[] buffer = new byte[Marshal.SizeOf(placement)];
+            using (BinaryWriter writer = new BinaryWriter(new MemoryStream(buffer))) {
+                writer.Write(placement.length);
+                writer.Write(placement.flags);
+                writer.Write(placement.showCmd);
+                writer.Write(placement.minPosition.X);
+                writer.Write(placement.minPosition.Y);
+                writer.Write(placement.maxPosition.Y);
+                writer.Write(placement.maxPosition.Y);
+                writer.Write(placement.normalPosition.Left);
+                writer.Write(placement.normalPosition.Top);
+                writer.Write(placement.normalPosition.Right);
+                writer.Write(placement.normalPosition.Bottom);
+            }
+            var brandKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"SOFTWARE\AkahaSoftware");
+            var key = brandKey.CreateSubKey("AkahaGesture");
+            key.SetValue("WindowPlacement", buffer, Microsoft.Win32.RegistryValueKind.Binary);
+            key.Close();
+            brandKey.Close();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e) {
+            base.OnSourceInitialized(e);
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"SOFTWARE\AkahaSoftware\AkahaGesture");
+            if (key != null) {
+                var currentPlacement = this.GetPlacement();
+                var placementBytes = (byte[])key.GetValue("WindowPlacement");
+                WINDOWPLACEMENT placement;
+                using (BinaryReader reader = new BinaryReader(new MemoryStream(placementBytes))) {
+                    try {
+                        placement.length = reader.ReadInt32();
+                        placement.flags = reader.ReadInt32();
+                        placement.showCmd = reader.ReadInt32();
+                        placement.minPosition.X = reader.ReadInt32();
+                        placement.minPosition.Y = reader.ReadInt32();
+                        placement.maxPosition.X = reader.ReadInt32();
+                        placement.maxPosition.Y = reader.ReadInt32();
+                        placement.normalPosition.Left = reader.ReadInt32();
+                        placement.normalPosition.Top = reader.ReadInt32();
+                        placement.normalPosition.Right = reader.ReadInt32();
+                        placement.normalPosition.Bottom = reader.ReadInt32();
+                    } catch(IOException) {
+                        // silently ignore decoding errors, and let the window open normally
+                        return;
+                    }
+                }
+                if (currentPlacement.showCmd != 1 /* SW_SHOWNORMAL */) {
+                    placement.showCmd = currentPlacement.showCmd;
+                }
+                this.SetPlacement(placement);
+            }
+        }
+
     }
 }
---
+++ WindowRestorerService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using System.Xml.Serialization;
+
+namespace Akaha_Gesture {
+    // RECT structure required by WINDOWPLACEMENT structure
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+    }
+
+    // POINT structure required by WINDOWPLACEMENT structure
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    // WINDOWPLACEMENT stores the position, size, and state of a window
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public POINT minPosition;
+        public POINT maxPosition;
+        public RECT normalPosition;
+    }
+
+    static class WindowRestorerService {
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+
+        private static Encoding encoding = new UTF8Encoding();
+        private static XmlSerializer serializer = new XmlSerializer(typeof(WINDOWPLACEMENT));
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPlacement(IntPtr hWnd, [In] ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowPlacement(IntPtr hWnd, out WINDOWPLACEMENT lpwndpl);
+
+        public static void SetPlacement(this Window window, WINDOWPLACEMENT placement)
+        {
+            WindowRestorerService.SetPlacement(new WindowInteropHelper(window).Handle, placement);
+        }
+
+        /// <summary>
+        /// Set the window to the relevent position using the placement information. 
+        /// </summary>
+        /// <param name="windowHandle">The window handle of the target information.</param>
+        /// <param name="placementXml">The placement XML.</param>
+        public static void SetPlacement(IntPtr windowHandle, WINDOWPLACEMENT placement)
+        {
+            SetWindowPlacement(windowHandle, ref placement);
+        }
+
+        public static WINDOWPLACEMENT GetPlacement(this Window window)
+        {
+            return WindowRestorerService.GetPlacement(new WindowInteropHelper(window).Handle);
+        }
+
+        /// <summary>
+        /// Retruns the serialize XML of the placement information for the 
+        /// target window.
+        /// </summary>
+        /// <param name="windowHandle">The handle of the target window.</param>
+        public static WINDOWPLACEMENT GetPlacement(IntPtr windowHandle)
+        {
+            WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+            GetWindowPlacement(windowHandle, out placement);
+            return placement;
+        }
+    }
+
+}
